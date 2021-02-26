86daf1db7800aae0ad30a22a74d61567e5cbc86b
alejandro1403 <orozco.alejandro@correounivalle.edu.co>
2021-02-26 00:24:52 -0500

    reflejo simple, intento modelo entorno

--- src/example/CleanerAgent.js
+++ src/example/CleanerAgent.js
@@ -7,25 +7,33 @@ const Agent = require('../core/Agent');
 class CleanerAgent extends Agent {
     constructor(value) {
         super(value);
-        //LEFT, UP, RIGHT, DOWN, CELL
+
         this.table = {
             "0,0,0,0,0": "UP",
             "0,0,0,1,0": "UP",
             "0,0,1,0,0": "UP",
-            "0,0,1,1,0": "LEFT",
-            "0,1,0,0,0": "LEFT",
-            "0,1,0,1,0": "RIGHT",
-            "0,1,1,0,0": "LEFT",
+            "0,0,1,1,0": "UP", //
+            "0,1,0,0,0": "DOWN",
+            "0,1,0,1,0": "RIGHT", //
+            "0,1,1,0,0": "DOWN",
             "0,1,1,1,0": "LEFT",
             "1,0,0,0,0": "UP",
-            "1,0,0,1,0": "RIGHT",
-            "1,0,1,0,0": "DOWN",
-            "1,0,1,1,0": "UP",
-            "1,1,0,0,0": "RIGHT",
-            "1,1,0,1,0": "RIGHT",
+            "1,0,0,1,0": "RIGHT", //
+            "1,0,1,0,0": "UP", //
+            "1,0,1,1,0": "UP", //
+            "1,1,0,0,0": "RIGHT", //
+            "1,1,0,1,0": "RIGHT", //
             "1,1,1,0,0": "DOWN",
             "default": "TAKE"
         };
+        /*//Mapa axuliar
+        globalThis.track = [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]]
+        */
+        globalThis.flag = "";
     }
 
     /**
@@ -33,19 +41,42 @@ class CleanerAgent extends Agent {
      * In this case, the state is just obtained as the join of the perceptions
      */
     send() {
-        let viewKey = this.perception.join();
+        
+        /*//llenar mapa auxiliar
+        var x = JSON.stringify(this.initialState).charAt(5)
+        var y = JSON.stringify(this.initialState).charAt(11)
+        track[y][x] = 1*/
+
+        let viewKey = this.perception;
+       
+        switch(flag){
+            case "UP":
+                viewKey[3] = 1;
+                break;
+            case "DOWN":
+                viewKey[1] = 1;
+                break;
+            case "LEFT":
+                viewKey[2] = 1;
+                break;
+            case "RIGHT":
+                viewKey[0] = 1;
+                break;
+            default:
+                break;
+        }
+
         //let action = foo(this.internalState, this.perception)
         //this.internalState = updatex(this.internalState, this.perception, action)
         //return action;
-
+        
         if (this.table[viewKey]) {
+            flag = this.table[viewKey];
             return this.table[viewKey];
         } else {
             return this.table["default"];
         }
-
-    }
-
+    };
 }
 
 module.exports = CleanerAgent;
